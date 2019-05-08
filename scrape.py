49d8ca8789ed689e743379fc49210520fff76052
Issa Rice <riceissa@gmail.com>
2019-05-08 10:32:37 +0100

    more stuff

--- scrape.py
+++ scrape.py
@@ -6,14 +6,21 @@ import sys
 
 from langmap import LANGMAP
 
+import pdb
 
 def main():
-    url = "https://stats.wikimedia.org/archive/squid_reports/2018-09/SquidReportPageViewsPerCountryBreakdownHuge.htm"
+    month = 201709
+    url = "https://stats.wikimedia.org/archive/squid_reports/2015-04/SquidReportPageViewsPerCountryBreakdownHuge.htm"
+    # SKIP 2016-11
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "lxml")
 
-    table = soup.find_all("table")[1]
+    # Use table index 2 if the report page is 2017-09 or older
+    if month <= 201709:
+        table = soup.find_all("table")[2]
+    else:
+        table = soup.find_all("table")[1]
     country = None
     for row in table.find_all("tr")[4:]:
         if row.find("th").get("class") == ['lh3']:
@@ -21,7 +28,9 @@ def main():
             country = row.find("th").a.get("name")
         elif row.find("th").get("class") == ['l']:
             language = row.find("th").text
-            if language == "Other":
+            if language == "Other" or language == "Portal" or language == "m Wp" or language == "Commons Wp" or language == "wwwhttp Wp" or language == "zero Wp" or language == "plhttp Wp":
+                # The portal thing shows up in https://stats.wikimedia.org/archive/squid_reports/2015-04/SquidReportPageViewsPerCountryBreakdownHuge.htm
+                # I'm not sure what "m Wp" means; maybe mobile?
                 continue
             assert language.endswith(" Wp")
             lang_code = LANGMAP[language[:-len(" Wp")]]
