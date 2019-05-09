708c197447e111e8c0b4c41e9c01949e3e511fec
Issa Rice <riceissa@gmail.com>
2019-05-09 17:09:55 +0200

    loop through months and print in sql

--- scrape.py
+++ scrape.py
@@ -9,38 +9,75 @@ from langmap import LANGMAP
 import pdb
 
 def main():
-    month = 201709
-    url = "https://stats.wikimedia.org/archive/squid_reports/2014-11/SquidReportPageViewsPerCountryBreakdownHuge.htm"
-    # SKIP 2016-11
+    year_months = []
+    year_months.append((2014, 12))
+    for year in range(2015, 2018):
+        for month in range(1, 13):
+            if not (year == 2016 and month == 11):
+                # SKIP 2016-11
+                year_months.append((year, month))
+    for month in range(1, 10):
+        year_months.append((2018, month))
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "lxml")
+    url_pattern = "https://stats.wikimedia.org/archive/squid_reports/{}-{:02d}/SquidReportPageViewsPerCountryBreakdownHuge.htm"
+    for year_month in year_months:
+        year, month = year_month
+        print("Doing year={}, month={:02d}".format(year, month), file=sys.stderr)
+        url = url_pattern.format(year, month)
 
-    # Use table index 2 if the report page is 2017-09 or older
-    if month <= 201709:
-        table = soup.find_all("table")[2]
-    else:
-        table = soup.find_all("table")[1]
-    country = None
-    for row in table.find_all("tr")[4:]:
-        if row.find("th").get("class") == ['lh3']:
-            # This means we have started a new country section
-            country = row.find("th").a.get("name")
-        elif row.find("th").get("class") == ['l']:
-            language = row.find("th").text
-            if language in ["Other", "Portal", "m Wp", "Commons Wp", "wwwhttp Wp", "zero Wp", "plhttp Wp", "enhttp Wp", "eshttp Wp"]:
-                # Portal, m Wp, Commons Wp, wwwhttp Wp, zero Wp, and plhttp Wp
-                # all show up in 2015-04; I'm not really sure what's up with
-                # these.
-                # https://stats.wikimedia.org/archive/squid_reports/2015-04/SquidReportPageViewsPerCountryBreakdownHuge.htm
-                continue
-            assert language.endswith(" Wp")
-            lang_code = LANGMAP[language[:-len(" Wp")]]
-            viewcount = row.find("td").text
-            print(country, lang_code, viewcount)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "lxml")
+
+        # Use table index 2 if the report page is 2017-09 or older
+        if year_month <= (2017, 9):
+            table = soup.find_all("table")[2]
         else:
-            print("we don't know what this means", file=sys.stderr)
-            raise ValueError
+            try:
+                table = soup.find_all("table")[1]
+            except IndexError:
+                pdb.set_trace()
+        country = None
+        print("insert into viewcountsbymonth(pagename,`language`,drilldown,`monthfull`,viewcount) values")
+        first = True
+        for row in table.find_all("tr")[4:]:
+            if row.find("th").get("class") == ['lh3']:
+                # This means we have started a new country section
+                country = row.find("th").a.get("name")
+            elif row.find("th").get("class") == ['l']:
+                language = row.find("th").text
+                if language in ["Other", "Portal", "m Wp", "Commons Wp", "wwwhttp Wp", "zero Wp", "plhttp Wp", "enhttp Wp", "eshttp Wp"]:
+                    # Portal, m Wp, Commons Wp, wwwhttp Wp, zero Wp, and plhttp Wp
+                    # all show up in 2015-04; I'm not really sure what's up with
+                    # these.
+                    # https://stats.wikimedia.org/archive/squid_reports/2015-04/SquidReportPageViewsPerCountryBreakdownHuge.htm
+                    continue
+                assert language.endswith(" Wp")
+                lang_code = LANGMAP[language[:-len(" Wp")]]
+                viewcount = row.find("td").text
+                print(("" if first else ",") + ",".join([
+                    mysql_quote(country),
+                    mysql_quote(lang_code),
+                    mysql_quote("country-total"),
+                    mysql_quote("{}{:02d}".format(year, month)),
+                    viewcount,
+                ]))
+                first = False
+            else:
+                print("we don't know what this means", file=sys.stderr)
+                raise ValueError
+        print(";\n")
+
+
+def mysql_quote(x):
+    """Quote the string x using MySQL quoting rules. If x is the empty string,
+    return "NULL". Probably not safe against maliciously formed strings, but
+    our input is fixed and from a basically trustable source."""
+    if not x:
+        return "NULL"
+    x = x.replace("\\", "\\\\")
+    x = x.replace("'", "''")
+    x = x.replace("\n", "\\n")
+    return "'{}'".format(x)
 
 
 
